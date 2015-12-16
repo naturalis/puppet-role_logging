f4755b8ab10b5a7e3c9fead4dbca9f3080a02758
Atze de Vries <atze.devries@naturalis.nl>
2015-12-16 10:26:35 +0100

    more timeout

--- manifests/elasticsearch_node.pp
+++ manifests/elasticsearch_node.pp
@@ -21,7 +21,8 @@ class role_logging::elasticsearch_node(
       'network.host'                         => '0.0.0.0',
       'discovery.zen.minimum_master_nodes'   => 1,
       'discovery.zen.ping.multicast.enabled' => false,
-      'discovery.zen.ping.unicast.hosts'     => $nodes_ip_array
+      'discovery.zen.ping.unicast.hosts'     => $nodes_ip_array,
+      'discovery.zen.ping.timeout'           => 30,
     },
     init_defaults => {
       'ES_HEAP_SIZE' => "${heapsize}g"
