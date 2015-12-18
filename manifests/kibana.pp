976e7a338131b129222b7675be77e25c7609b673
Atze de Vries <atze.devries@naturalis.nl>
2015-12-18 10:38:51 +0100

    asdg

--- manifests/kibana.pp
+++ manifests/kibana.pp
@@ -2,7 +2,8 @@
 #
 #
 class role_logging::kibana(
-  $kibana_link = 'https://download.elastic.co/kibana/kibana/kibana-4.3.1-linux-x64.tar.gz'
+  $kibana_link = 'https://download.elastic.co/kibana/kibana/kibana-4.3.1-linux-x64.tar.gz',
+  $elasticsearch_host = '127.0.0.1'
 ){
 
 
@@ -10,6 +11,11 @@ class role_logging::kibana(
   staging::deploy {'kibana-4.3.1-linux-x64.tar.gz':
     source => $kibana_link,
     target => '/opt/'
+  }
+
+  file_line {'kibana_es_host_config':
+    path => '/opt/kibana-4.3.1-linux-x64/config/kibana.yaml',
+    line => "elasticsearch.url: ${elasticsearch_host}:9200",
   }
 
 }
