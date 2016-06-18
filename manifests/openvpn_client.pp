5781f10d53ae78c923a9f26dc7484c3a47c412d5
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-06-18 19:50:31 +0200

    Delete openvpn_client.pp

--- manifests/openvpn_client.pp
+++
@@ -1,9 +0,0 @@
-# == Class: role_logging::openvpn_client
-#
-class role_logging::openvpn_client (
-  $openvpn_client_hash,
-) {
-
-  create_resources('openvpn_client::client', $openvpn_client_hash)
-
-}
