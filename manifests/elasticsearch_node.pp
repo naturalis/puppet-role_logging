6f4bbc5f5725b52ca36fdee315f378dbd2659983
Atze de Vries <atze.devries@naturalis.nl>
2016-11-04 11:00:31 +0100

    updated es version from 2.1.0 to 2.1.2

--- manifests/elasticsearch_node.pp
+++ manifests/elasticsearch_node.pp
@@ -20,7 +20,7 @@ class role_logging::elasticsearch_node(
   }
 
   class { 'elasticsearch':
-    package_url   => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.1.0/elasticsearch-2.1.0.deb',
+    package_url   => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.1.2/elasticsearch-2.1.2.deb',
     java_install  => true,
     config        => {
       'node.name'                            => $::hostname,
